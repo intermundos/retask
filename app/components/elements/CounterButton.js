d00f930e300e3e09208b27f937e002acfde1279f
Anton Stroinsky <anton.st@matomy.com>
2017-02-27 11:56:02 +0200

    First commit

--- app/components/elements/CounterButton.js
+++ app/components/elements/CounterButton.js
@@ -1,6 +1,6 @@
 import React        from 'react';
 
-import './button.scss';
+import './CounterButton.scss';
 
 const ButtonView = ({ text, onClick }) => (
 
--- app/components/pages/Home.js
+++ app/components/pages/Home.js
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, {Component, PropTypes} from 'react';
+import  axios   from      'axios';
+import  mapVal   from      'lodash/mapValues';
 import './Home.scss';
 
 
-const Home = () => {
-	return (
-		<div className="page page--home">
-			<div className="container">
+class Home extends Component {
+    constructor(props) {
+        super(props);
 
-			</div>
-		</div>
-	)
-};
+        this.state = {
+            data: [],
+            userName: '',
+            userEmail: '',
+            userPass: ''
+        };
+
+        this.fetchData = this.fetchData.bind(this);
+    }
+
+    fetchData() {
+        axios.get('http://interspace.dev/wp-json/acf/v2/page/5/repeat')
+            .then((response) => {
+             
+                const result = response.data.repeat;
+                console.log(response.data.repeat);
+                this.setState({
+                    data: result
+                })
+            })
+
+    }
+
+
+
+    render() {
+        return (
+            <div className="page page--home">
+                <div className="container">
+                    <button className="button is-success is-large"
+                            onClick={ this.fetchData }
+
+                    >
+                        { `Fetch data` }
+                    </button>
+
+                    <div>
+                        {
+                            this.state.data.map((item, index)=> 
+                                <div className="notification is-info" key={index}>
+                                    <div className="text">{item.text}</div>
+                                    { item.image != false ? <img src={ item.image }
+                                                                 className="thumb"
+                                                                 style={{maxWidth: '100px' }}/> : null }
+                                </div>)
+                        }
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Home;
